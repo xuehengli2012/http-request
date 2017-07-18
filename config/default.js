87cafb64d39694795e9ca9b95cf235a2392e51e1
xuehengli2012 <xuehengli2012@gmail.com>
2017-07-18 17:16:55 +0800

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
-   appId           : '29172a9c-c893-456a-bc8c-648132e35390',
-   clientId        : 'myclientid',
-   clientSecret    : 'myclientsecret',
-   appSignature    : 'myappsignature',
+   appId           : '1ebe8921-6a13-46b0-86f0-ea3bccc25ef0',
+   clientId        : 'pag33w1rdsl6xl72uw2nomtb',
+   clientSecret    : '0nscKloYFCrnkL1AwhYaA4Ad',
+   appSignature    : 'zdcwwr3imuxgqhpvlohhzlvas4o1z2l3emjhubsezpjaoqzdmyetjc100pt2kwyeyut034v2glu5sqst11fxeld4qjcylqu4wikahg0443rab0teuz3byt5gqh1bxndohxobpl1pr10j0mf34i2k1px03quw5mm2psc4pjvxxrf4tinjvtxsc35amgzwqd34gk1bcorsqigddohtbf3s5qcsduypte4z4hzyewp24j52j4ilkoumsqkyzmptag0',
    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
