163f628443f77e004aa8859be7f70bd35d61feab
xuehengli2012 <xuehengli2012@gmail.com>
2017-07-18 17:18:24 +0800

    Update default.js

--- config/default.js
+++ config/default.js
@@ -5,4 +5,3 @@ module.exports = {
    appSignature    : 'zdcwwr3imuxgqhpvlohhzlvas4o1z2l3emjhubsezpjaoqzdmyetjc100pt2kwyeyut034v2glu5sqst11fxeld4qjcylqu4wikahg0443rab0teuz3byt5gqh1bxndohxobpl1pr10j0mf34i2k1px03quw5mm2psc4pjvxxrf4tinjvtxsc35amgzwqd34gk1bcorsqigddohtbf3s5qcsduypte4z4hzyewp24j52j4ilkoumsqkyzmptag0',
    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
-
